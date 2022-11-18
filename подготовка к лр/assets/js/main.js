8884c69e3f9c35aeefa059a1cf167708078a7ebf
lowage24 <114925366+lowage24@users.noreply.github.com>
2022-11-18 15:40:38 +0300

    Add files via upload

---
+++ подготовка к лр/assets/js/main.js
@@ -0,0 +1,51 @@
+// 1
+// function js_style() 
+// {
+// let text = document.getElementById('text');
+//  text.style.fontSize = "14pt";
+//  text.style.fontFamily = "Comic Sans MS";
+//  text.style.color = "green";
+//  text.style.border = '5px solid green';
+//  text.style.width = '1000px';
+//  text.style.height = '1000px';
+//  text.style.background = 'purple';
+// }
+
+// 2
+//  function getFormvalue(event) {
+//     event.preventDefault()
+ 
+//   var first = document.getElementById("Fname");
+//   var second = document.getElementById("Iname");
+//   let div = document.createElement('div');
+//   div.innerHTML = first.value+" "+second.value
+//   document.body.append(div); 
+// }
+
+// 3
+// function getAttributes(){
+//     let link = document.getElementById('w3r');
+//     let arr = [link.href,link.hreflang,link.rel,link.target,link.type]
+//     let div = document.createElement('div');
+//     for (let i = 0; i<arr.length;i++){
+//     div.innerHTML += '<p class = "paragraph">'+arr[i]+'</p>'
+
+//     }
+//     document.body.append(div);
+//     document.body.innerHTML += '<style>.paragraph{color:brown;font-size: 50px;}</style>'
+// }
+
+// function insert_Row()
+// {
+// var x=document.getElementById('sampleTable').insertRow(0);
+// var y = x.insertCell(0);
+// var z = x.insertCell(1);
+// y.innerHTML="New Cell1";
+// z.innerHTML="New Cell2";
+// let elem = document.getElementById('sampleTable')
+// elem.style.border = 'yellow'
+// elem.style.background = 'blue'
+// }
+
+
+ 
